3e99003f5700cf8a1995a5df4e58216bcfe006ba
dvojtko <danovojtko@gmail.com>
2025-11-27 19:22:20 +0100

    Use log level and format from settings

--- bingo/run.py
+++ bingo/run.py
@@ -1,9 +1,12 @@
+import logging
+
 import uvicorn
 
 from bingo.settings import settings
 
 
 def main() -> None:
+    logging.basicConfig(level=settings.LOG_LEVEL, format=settings.LOGGING_FORMAT)
     uvicorn.run(
         "bingo.api:app",
         host=settings.HOST,
